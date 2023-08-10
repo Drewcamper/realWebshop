adc0ba975e3b5082227689a77eceeb408f0d8dbd
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2023-08-10 12:55:22 +0200

    version 1.0. Responsive design should be checked again!

--- client/src/components/main/Welcome.js
+++ client/src/components/main/Welcome.js
@@ -1,25 +1,13 @@
 import React from "react";
 import "../../style/main/welcome.css";
 
-const Webshop = () => {
-  return (
-    <>
-      <div className="side">Webshop!</div>
-      <div className="side back">Webshop!</div>
-      <div className="side front">Webshop!</div>
-    </>
-  );
-};
-
 function Welcome() {
   return (
     <>
       <div className="welcomeWrapper">
         <div className="welcomeTextWrapper">
           <div className="welcomeFlexbox">
-              <div className="welcome firstWord">Welcome </div>
-              <div className="welcome to">to </div>
-              <div className="welcome my">my </div>
+            <div className="welcome firstLine">Welcome to my</div>{" "}
           </div>
           <div className="welcome portfolio">portfolio </div>
           <div className="welcome webshop">Webshop!</div>
@@ -30,7 +18,6 @@ function Welcome() {
           meaningful experiences.
         </div>
       </div>
-      {/* <RotatingColumn /> */}
     </>
   );
 }
